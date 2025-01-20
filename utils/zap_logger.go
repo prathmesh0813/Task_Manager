e9f5a365bcd9323bd92d0d0fbac913f55ec02e79
Amjad Ali <amjadmuz27@gmail.com>
2025-01-20 16:28:10 +0000

    issue solved for creating log files

--- utils/zap_logger.go
+++ utils/zap_logger.go
@@ -36,7 +36,7 @@ var Logger *zap.Logger
 
 func InitLogger() {
 
-	logFileName := fmt.Sprintf("logs_%s.log", time.Now().Format("2006-01-02_15-04-05"))
+	logFileName := fmt.Sprintf("logs_%s.log", time.Now().Format("2006-01-02"))
 	logFile, err := os.OpenFile(logFileName, os.O_APPEND|os.O_CREATE|os.O_WRONLY, 0666)
 	if err != nil {
 		panic(fmt.Sprintf("Failed to create log file: %v", err))
