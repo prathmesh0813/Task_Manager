158f661c22189b13ad86942a35a708085e4b2ba7
prathmesh0813 <prathmesh.gon@gmail.com>
2025-01-25 17:46:55 +0530

    response formatter middleware separated

--- middlewares/auth.go
+++ middlewares/auth.go
@@ -11,26 +11,7 @@ import (
 	"go.uber.org/zap"
 )
 
-// Set the response in gin context
-func ResponseFormatter() gin.HandlerFunc {
-	return func(c *gin.Context) {
-		c.Next()
 
-		response, _ := c.Get("response")
-		message, _ := c.Get("message")
-
-		errorStatus, _ := c.Get("error")
-
-		formattedResponse := gin.H{
-			"message": message,
-			"error":   errorStatus,
-			"data":    response,
-		}
-
-		c.JSON(c.Writer.Status(), formattedResponse)
-
-	}
-}
 
 // Checks whether the user is authenticated to perform the action
 func Authenticate(c *gin.Context) {
